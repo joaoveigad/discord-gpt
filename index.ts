ee0bf6ffd178d225a74ac2c05cce133b7369dda9
João <jvdvandrade@outlook.pt>
2025-03-17 16:26:36 -0300

    listener de comandos

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-import { Collection } from 'discord.js';
+import { Collection, MessageFlags } from 'discord.js';
 require('dotenv/config');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -26,7 +26,6 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
-
     const commandsPath = path.join(foldersPath, folder);
     console.log(`${commandsPath}, lancem a Millia no Strive`)
     const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.js'));
@@ -41,9 +40,37 @@ for (const folder of commandFolders) {
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
         }
     }
-
 }
 
+client.on(Events.InteractionCreate, async (interaction: any) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`Command ${interaction.commandName} not found`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command.',
+                flags: MessageFlags.Ephemeral
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command.',
+                flags: MessageFlags.Ephemeral
+            });
+        }
+    }
+
+    console.log(interaction);
+});
 
 
 
