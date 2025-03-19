18dde598bd01fd2ad91ded82600761391d3e3b25
João <jvdvandrade@outlook.pt>
2025-03-19 20:07:37 -0300

    fix de verdade

--- commands/utility/bot.js
+++ commands/utility/bot.js
@@ -13,6 +13,8 @@ module.exports = {
 
     async execute(interaction) {
         const message = interaction.options.getString('input');
+        await interaction.reply('Pensando!')
+        
         const openai = new OpenAI({
             apiKey: process.env.OPENAI_KEY,
         });
@@ -20,13 +22,13 @@ module.exports = {
         try {
             const completion = await openai.chat.completions.create({
                 messages: [{ role: "user", content: message }],
-                model: "gpt-4o-mini",
+                model: "gpt-4o",
                 store: true,
             });
             
             console.log(completion.choices[0])
-            await interaction.reply('Pensando!')
-            const edit = completion.choices[0].message.content
+
+            const edit = await completion.choices[0].message.content
             await interaction.editReply(edit);
 
         } catch (error) {
